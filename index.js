5b9961a993c363e8d0493db0835eceff912a2409
Crista Cooper <aine3d@gmail.com>
2017-09-14 01:39:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function iLoveTheBeatles(number) {
   do {
     array.push("I love the Beatles!");
     number++;
-  } while (i <= number && number < 15) {
+  } while (i <= number && number < 15) ;
 return array;
-}
 }
